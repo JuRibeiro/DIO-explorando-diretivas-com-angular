399b20b3aae6c605a94ad902c4e9133bffed5336
JuRibeiro <ju.ribeiro1208@gmail.com>
2021-12-20 12:51:24 -0300

    diretivas de atributos, estruturais e customizadas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,66 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'my-second-project';
+export class AppComponent implements OnInit{
+  count = 0;
+  nome = 'Juliana Brito Favero Ribeiro';
+  text = '';
+
+  pessoas = [
+    {
+      nome: "Juliana",
+      sobrenome: "Ribeiro"
+    },
+    {
+      nome: "Ana Julia",
+      sobrenome: "Favero"
+    },
+    {
+      nome: "Julia",
+      sobrenome: "Brito"
+    },
+    {
+      nome: "Julia",
+      sobrenome: "Costa"
+    }
+  ];
+
+  constructor()
+  {
+
+  }
+  ngOnInit() {
+    console.log(this.pessoas);
+
+    let intervalo = setInterval(()=>
+    {
+      this.count++;
+      if(this.count === 10)
+      {
+        clearInterval(intervalo);
+      }
+    }, 1000)
+  }
+
+  clicou (nome: string): void
+  {
+    console.log('clicou aqui ', nome)
+  }
 }
+
+/* Ciclo de vida angular
+
+Constructor
+ngOnChange *
+ngOnInit
+ngDoCheck *
+ngAfterContentInit
+ngAfterContentChecked *
+ngAfterViewInit
+ngAfterViewChecked *
+ngOnDestroy (limpar uma chamada)
+ */
